20fb31a4c9054f3d65a8f397392d88efb96f2c3c
Ashley <ashleycheung2412@gmail.com>
2021-09-16 12:10:57 +1000

    Updated version

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const VERSION = '1.0.3';
+const VERSION = '1.0.4';
 
 console.log(`Flow forms version: ${VERSION}`);
 
